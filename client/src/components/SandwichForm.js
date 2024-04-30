0f664b69f9549187775ec6b77f6c89fd208f8eb9
Sean Coleman <seancoleman725@gmail.com>
2024-04-30 17:05:26 -0400

    Add new sandwich to the list

---
+++ client/src/components/SandwichForm.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react"
+
+const SandwichForm = (props) => {
+  const [sandwich, setSandwich] = useState({
+    name: "",
+    restaurant: "",
+    description: "",
+    imageURL: ""
+  })
+
+  const onChangeHandler = event => {
+    setSandwich({
+      ...sandwich,
+      [event.currentTarget.name]: event.currentTarget.value
+    })
+  }
+  const clearForm = (event) => {
+    event.preventDefault()
+    setSandwich({
+      name: "",
+      restaurant: "",
+      description: "",
+      imageURL: ""
+    })
+    props.setErrors({})
+  }
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault()
+    props.addSandwich(sandwich)
+    setSandwich({
+      name: "",
+      restaurant: "",
+      description: "",
+      imageURL: ""
+    })
+  }
+
+  return (
+    <form className="callout" onSubmit={onSubmitHandler}>
+      <label htmlFor="name">Sandwich Name:
+        <input 
+          type="text" 
+          name="name" 
+          onChange={onChangeHandler} 
+          value={sandwich.name}
+        />
+      </label>
+      <label htmlFor="restaurant">Restaurant Name:
+        <input 
+          type="text" 
+          name="restaurant"
+          onChange={onChangeHandler} 
+          value={sandwich.restaurant}
+        />
+      </label>
+      <label htmlFor="description">Description:
+        <textarea 
+          name="description" 
+          rows="4" 
+          cols="50"
+          onChange={onChangeHandler}
+          value={sandwich.description}
+        />
+      </label>
+      <label htmlFor="ImageURL">Image URL:
+        <input 
+          type="text" 
+          name="imageURL"
+          onChange={onChangeHandler} 
+          value={sandwich.imageURL}
+        />
+      </label>
+      <div className="button-group">
+        <input type="submit" value="Create Sandwich"/>
+        <button onClick={clearForm} type="button">
+          Clear
+        </button>
+      </div>
+    </form>
+  )
+}
+
+export default SandwichForm
--- client/src/components/SandwichList.js
+++ client/src/components/SandwichList.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect } from "react"
 import SandwichTile from "./SandwichTile"
+import SandwichForm from "./SandwichForm"
+import ErrorList from "./ErrorList"
+import translateServerErrors from "../services/translateServerErrors"
 
 const SandwichList = (props) => {
   const [sandwiches, setSandwiches] = useState([])
+  const [showForm, setShowForm] = useState(false)
+  const [errors, setErrors] = useState({})
 
   const getSandwiches = async () => {
     try {
@@ -14,18 +19,70 @@ const SandwichList = (props) => {
       console.log(error)
     }
   }
+
+  const addSandwich = async (newSandwich) => {
+    try {
+      const response = await fetch("/api/v1/sandwiches", {
+        method: "POST",
+        headers: new Headers ({
+          "Content-Type": "application/json"
+        }),
+        body: JSON.stringify(newSandwich)
+      })
+      if (!response.ok) {
+        if (response.status === 422) {
+          const errorBody = await response.json()
+          const newErrors = translateServerErrors(errorBody.errors)
+          return setErrors(newErrors)
+        } else {
+          const errorMessage = `${response.status} (${response.statusText})`
+          const error = new Error(errorMessage)
+          throw error
+        }
+    } else {
+      const responseBody = await response.json()
+      setSandwiches([
+        ...sandwiches,
+        responseBody.newSandwich
+      ])
+    }
+    } catch (error) {
+      console.error(`Error in fetch: ${error.message}`)
+    }
+  }
   
   useEffect(() => {
     getSandwiches()
   }, [])
 
+  const toggleFormOnClick = event => {
+    setShowForm(true)
+  }
+
+  const formQuestion = 
+    (
+        <>
+          <ErrorList errors={errors}/>
+          <SandwichForm addSandwich={addSandwich} setErrors={setErrors}/>
+        </>
+    )
+  
+  let formElement  
+  if (showForm) {
+    formElement = formQuestion
+  }
+
   const sandwichList = sandwiches.map(sandwich => {
-    return <SandwichTile key={sandwich.id} sandwich={sandwich} />
+    return <SandwichTile key={sandwich.id} sandwich={sandwich}/>
   })
 
   return (
     <>
       {sandwichList}
+      <div>
+        <p onClick={toggleFormOnClick}>Add Sandwich?</p>
+        {formElement}
+      </div>
     </>
   )
 }
